9bd7f9e13f32942b29a5c8c6dac5ab71ba3d3e68
SebastianProdaso <s.heger@prodaso.de>
2020-10-03 10:55:46 +0200

    adds docker support for the database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'project_database',
+        'NAME': 'webframeworkdb',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'localhost',
